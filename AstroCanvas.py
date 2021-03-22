1e9a608453f428bda64343456b69a68a8fcccb84
piexpiex <57443061+piexpiex@users.noreply.github.com>
2021-03-22 13:08:47 +0100

    Add files via upload

--- AstroCanvas.py
+++ AstroCanvas.py
@@ -1,11 +1,14 @@
 import numpy as np
-from modes.RGB_MAKER import *
-from modes.GREY_MAKER import *
 from modes.BIAS import *
 from modes.DARKS import *
 from modes.FLATS import *
-from modes.CALIBRATION import *
+from modes.REDUCTION import *
 from modes.SPECTRAL_ARCS import *
+from modes.SPECTRA import *
+from modes.RGB_MAKER import *
+from modes.GREY_MAKER import *
+from modes.CONTOUR_MAP import *
+from modes.LIGHTCURVE_MAKER import *
 import matplotlib.pyplot as plt
 from matplotlib import rcParams
 from matplotlib.colors import BoundaryNorm
@@ -17,6 +20,7 @@ from matplotlib.backends.backend_tkagg import (
     FigureCanvasTkAgg, NavigationToolbar2Tk)
 from matplotlib.backend_bases import key_press_handler
 from matplotlib.figure import Figure
+from astropy.modeling import models, fitting
 
 class App:
 	def __init__(self, master,imagen,icono):
@@ -37,6 +41,21 @@ class App:
 		self.basic_exit_get=Button(master,text='exit',width=10,command=master.quit,bg='grey')
 		self.basic_exit_get.pack()
 		self.basic_exit_get.place(x=1100,y=0)
+		
+		#initial canvas
+		
+		fig, ax = plt.subplots()
+		#fig.set_size_inches(4.5, 4.5)
+		#axoff_fun = np.vectorize(lambda ax:ax.axis('off'))
+		# ... stuff here ...
+		#axoff_fun(ax)
+		#fig.savefig('test2png.png', dpi=100)
+		#self.Canvas_text=Label(master,text='CURRENTLY IMAGE',width=64,bg='grey')
+		#self.Canvas_text.pack()
+		#self.Canvas_text.place(x=400,y=100)
+		#self.canvas = FigureCanvasTkAgg(fig, master=master)  # A tk.DrawingArea.
+		#self.canvas.draw()
+		#self.canvas.get_tk_widget().place(x=400,y=150)
 		
 		#Select theme
 		def _theme(*event):       #colores de los botones y de las letras
@@ -69,7 +88,7 @@ class App:
 		self.basic_mode_text=Label(master,text='Mode',width=10,bg='grey')
 		self.basic_mode_text.pack()
 		self.basic_mode_text.place(x=10,y=150,height=25)
-		self.basic_mode_get = OptionMenu(master, mode,'bias','darks','flats','image reduction','espectral arcs', 'rgb images', 'grey images')
+		self.basic_mode_get = OptionMenu(master, mode,'bias','darks','flats','image reduction','spectral arcs','spectra', 'rgb images', 'grey images','contour map','lightcurve')
 		self.basic_mode_get.config(width=15,bg='grey',highlightthickness=0)
 		self.basic_mode_get.pack()
 		self.basic_mode_get.place(x=100,y=150,height=25)
@@ -100,25 +119,33 @@ class App:
 			elif mode.get()=='image reduction':
 				_=destroy_app(self)
 				_=CALIBRATION_IMAGES(self,master)
-			elif mode.get()=='espectral arcs':
+			elif mode.get()=='spectral arcs':
 				_=destroy_app(self)
 				_=SPECTRAL_ARCS(self,master)
+			elif mode.get()=='spectra':
+				_=destroy_app(self)
+				_=SPECTRA(self,master)
 			elif mode.get()=='rgb images':
 				_=destroy_app(self)
 				_=RGB_IMAGES(self,master)
 			elif mode.get()=='grey images':
 				_=destroy_app(self)
 				_=GREY_IMAGES(self,master)
-			
+			elif mode.get()=='contour map':
+				_=destroy_app(self)
+				_=CONTOUR_MAP(self,master)
+			elif mode.get()=='lightcurve':
+				_=destroy_app(self)
+				_=LIGHTCURVE(self,master)
 		
 		mode.trace('w',_mode)
 
 		
-		
-		self.basic_destroy = Button(master,text="destroy",width=10,command=self.destroy_app,bg='grey')
-		self.basic_destroy.pack()
-		self.basic_destroy.place(x=1100,y=30)
-		print(vars(self))
+		print('aa')
+		#self.basic_destroy = Button(master,text="destroy",width=10,command=self.destroy_app,bg='grey')
+		#self.basic_destroy.pack()
+		#self.basic_destroy.place(x=1100,y=30)
+		#print(vars(self))
 		
 	def destroy_app(self):
 		for var in vars(self):
