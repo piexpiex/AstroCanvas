6049d996eb4f3a301aee9019dc473a7194210731
piexpiex <57443061+piexpiex@users.noreply.github.com>
2021-04-13 13:10:39 +0200

    Add files via upload

--- modes/LIGHTCURVE_MAKER.py
+++ modes/LIGHTCURVE_MAKER.py
@@ -2,136 +2,265 @@ import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib import rcParams
 from astropy.io import fits
-from astropy.visualization import make_lupton_rgb
+from astropy.timeseries import BoxLeastSquares,TimeSeries,aggregate_downsample
+from astropy.time import Time
+from astropy import units as u
+from astropy.table import Table
 from tkinter import *
 import sys
 from matplotlib.backends.backend_tkagg import (FigureCanvasTkAgg, NavigationToolbar2Tk)
 from matplotlib.backend_bases import key_press_handler
 from matplotlib.figure import Figure
 
-def LIGHTCURVE(self,master):
-	#-------estimation of master bias image----------
-	
-	
-	self.MASTER_BIAS_MAKER_text=Label(master,text='MASTER BIAS MAKER',width=45,bg='grey')
-	self.MASTER_BIAS_MAKER_text.pack()
-	self.MASTER_BIAS_MAKER_text.place(x=10,y=350)
-	self.BIAS_text=Label(master,text='bias images',width=10,bg='grey')
-	self.BIAS_text.pack()
-	self.BIAS_text.place(x=10,y=380)
-	self.BIAS_get=Entry(master,bg='grey',width=40)
-	self.BIAS_get.insert(0,'bias.ls')
-	self.BIAS_get.pack()
-	self.BIAS_get.place(x=90,y=380)
+import warnings
+warnings.filterwarnings('ignore')
 
-	def read_image(image):
-		image_name=str(image)
-		if image_name[len(image_name)-5:]=='.fits':
-			image_data=fits.open(image_name)[0].data
+def LIGHTCURVE(self,master):
+	#-------creation of lightcurve from a fits table----------
+	
+	self.TPERIOD_text=Label(master,text='period',bg='grey')
+	self.TPERIOD_text.pack()
+	self.TPERIOD_text.place(x=900,y=200,width=70,height=22)
+	self.TPERIOD_get=Entry(master,bg='grey')
+	self.TPERIOD_get.insert(0,'')
+	self.TPERIOD_get.pack()
+	self.TPERIOD_get.place(x=975,y=200,width=210,height=22)
+	
+	self.TSTART_text=Label(master,text='time start',bg='grey')
+	self.TSTART_text.pack()
+	self.TSTART_text.place(x=900,y=228,width=70,height=22)
+	self.TSTART_get=Entry(master,bg='grey')
+	self.TSTART_get.insert(0,'')
+	self.TSTART_get.pack()
+	self.TSTART_get.place(x=975,y=228,width=210,height=22)
+	
+	self.NBINS_text=Label(master,text='bins number',bg='grey')
+	self.NBINS_text.pack()
+	self.NBINS_text.place(x=900,y=256,width=70,height=22)
+	self.NBINS_get=Entry(master,bg='grey')
+	self.NBINS_get.insert(0,'')
+	self.NBINS_get.pack()
+	self.NBINS_get.place(x=975,y=256,width=210,height=22)
+	
+	self.MIN_PERIOD_text=Label(master,text='min period',bg='grey')
+	self.MIN_PERIOD_text.pack()
+	self.MIN_PERIOD_text.place(x=900,y=284,width=70,height=22)
+	self.MIN_PERIOD_get=Entry(master,bg='grey')
+	self.MIN_PERIOD_get.insert(0,'')
+	self.MIN_PERIOD_get.pack()
+	self.MIN_PERIOD_get.place(x=975,y=284,width=65,height=22)
+	
+	self.MAX_PERIOD_text=Label(master,text='max period',bg='grey')
+	self.MAX_PERIOD_text.pack()
+	self.MAX_PERIOD_text.place(x=1045,y=284,width=70,height=22)
+	self.MAX_PERIOD_get=Entry(master,bg='grey')
+	self.MAX_PERIOD_get.insert(0,'')
+	self.MAX_PERIOD_get.pack()
+	self.MAX_PERIOD_get.place(x=1120,y=284,width=65,height=22)
+	
+	self.PERIOD_BINS_text=Label(master,text='period bins',bg='grey')
+	self.PERIOD_BINS_text.pack()
+	self.PERIOD_BINS_text.place(x=900,y=312,width=70,height=22)
+	self.PERIOD_BINS_get=Entry(master,bg='grey')
+	self.PERIOD_BINS_get.insert(0,'')
+	self.PERIOD_BINS_get.pack()
+	self.PERIOD_BINS_get.place(x=975,y=312,width=210,height=22)
+
+	self.LIGHTCURVE_MAKER_text=Label(master,text='LIGHTCURVE MAKER',width=45,bg='grey')
+	self.LIGHTCURVE_MAKER_text.pack()
+	self.LIGHTCURVE_MAKER_text.place(x=10,y=350)
+	self.LIGHTCURVE_text=Label(master,text='bias images',width=10,bg='grey')
+	self.LIGHTCURVE_text.pack()
+	self.LIGHTCURVE_text.place(x=10,y=380)
+	self.LIGHTCURVE_get=Entry(master,bg='grey',width=40)
+	self.LIGHTCURVE_get.insert(0,'lightcurve.fits')
+	self.LIGHTCURVE_get.pack()
+	self.LIGHTCURVE_get.place(x=90,y=380)
+	
+	self.EXTENSION_text=Label(master,text='extension',width=10,bg='grey')
+	self.EXTENSION_text.pack()
+	self.EXTENSION_text.place(x=10,y=410)
+	self.EXTENSION_get=Entry(master,bg='grey',width=40)
+	self.EXTENSION_get.insert(0,'VALUES')
+	self.EXTENSION_get.pack()
+	self.EXTENSION_get.place(x=90,y=410)
+	
+	self.TIME_COLUMN_text=Label(master,text='times',width=10,bg='grey')
+	self.TIME_COLUMN_text.pack()
+	self.TIME_COLUMN_text.place(x=10,y=440)
+	self.TIME_COLUMN_get=Entry(master,bg='grey',width=40)
+	self.TIME_COLUMN_get.insert(0,'time')
+	self.TIME_COLUMN_get.pack()
+	self.TIME_COLUMN_get.place(x=90,y=440)
+	
+	self.INTENSITY_COLUMN_text=Label(master,text='intensity',width=10,bg='grey')
+	self.INTENSITY_COLUMN_text.pack()
+	self.INTENSITY_COLUMN_text.place(x=10,y=470)
+	self.INTENSITY_COLUMN_get=Entry(master,bg='grey',width=40)
+	self.INTENSITY_COLUMN_get.insert(0,'intensity')
+	self.INTENSITY_COLUMN_get.pack()
+	self.INTENSITY_COLUMN_get.place(x=90,y=470)
+
+	def read_table(image,extension,times,intensity):  #(LIGHTCURVE_images[j],LIGHTCURVE_extension,LIGHTCURVE_times,LIGHTCURVE_intensity)
+		if intensity==0:
+			image_name=str(image)
+			if image_name[len(image_name)-5:]=='.fits':
+				image_data=fits.open(image_name)[extension].data
+				
+				hdul = fits.open(image_name)
+				image_data = hdul[extension].data
+				hdul.close()
+				image_times=image_data.field(times)
+			else:
+				print('the format of the image is not avalaible \n please try with one of these formats: \n     FITS')
+				
+			return(image_times) 
 		else:
-			print('the format of the image is not avalaible \n please try with one of these formats: \n     FITS') 
-		return(image_data)
+			image_name=str(image)
+			if image_name[len(image_name)-5:]=='.fits':
+				image_data=fits.open(image_name)[extension].data
+				
+				hdul = fits.open(image_name)
+				image_data = hdul[extension].data
+				hdul.close()
+				image_times=image_data.field(times)
+				image_intensities=image_data.field(intensity)
+			else:
+				print('the format of the image is not avalaible \n please try with one of these formats: \n     FITS')
+				
+			return(image_times,image_intensities) 
+		
 	def delete_space(A):
-		while A[0]==' ':
-			A=A[1:len(A)]
-		while A[len(A)-1]==' ':
-			A=A[0:len(A)-1]
+		if len(A)<2:
+			pass
+		else:
+			while A[0]==' ' and len(A)>1:
+				A=A[1:len(A)]
+			while A[len(A)-1]==' ' and len(A)>1:
+				A=A[0:len(A)-1]
 		return(A)
 	def read_list(file_list):
-		image_list=[]
-		fichero=open(file_list)
-		for linea in fichero:
-			linea=linea[0:len(linea)-1]
-			image_list.append(delete_space(linea))
+		if file_list[len(file_list)-4:]=='fits':
+			image_list=[file_list]
+		else:
+			image_list=[]
+			fichero=open(file_list)
+			for linea in fichero:
+				linea=linea[0:len(linea)-1]
+				image_list.append(delete_space(linea))
 		return(image_list)
-	def bias_image(*event):
+	def lightcurve(*event):
 		#destroy previous canvas to save memory
+		try:
+			_=self.canvas.toolbar.destroy()
+		except:
+			pass
 		try:
 			_=self.canvas.get_tk_widget().destroy()
 		except:
 			pass
-		global MASTER_BIAS
-		global MASTER_BIAS_std
-		BIAS_list=self.BIAS_get.get()
-		BIAS_images=read_list(BIAS_list)
-		for j in range(len(BIAS_images)):
-			if j==0:
-				BIAS_data=np.array(read_image(BIAS_images[j]))
-			else:
-				BIAS_data=np.dstack((BIAS_data,np.array(read_image(BIAS_images[j]))))
-
-		#Cut image
-		BIAS_x_1,BIAS_x_2,BIAS_y_1,BIAS_y_2=self.Xmin_value.get(),self.Xmax_value.get(),self.Ymin_value.get(),self.Ymax_value.get()
-		#Selection of combining method
-		combining_method=combining.get()
 		
-		if combining_method=='average':
-			MASTER_BIAS=np.mean(BIAS_data,axis=2)
-			print(np.shape(MASTER_BIAS))
-			MASTER_BIAS_std=np.std(BIAS_data,axis=2)
-			print(MASTER_BIAS)
-		elif combining_method=='median':
-			MASTER_BIAS=np.median(BIAS_data,axis=2)
-			print(np.shape(MASTER_BIAS))
-			MASTER_BIAS_std=np.std(BIAS_data,axis=2)
-			print(MASTER_BIAS)
-		try:
-			MASTER_BIAS=MASTER_BIAS[int(BIAS_x_1):int(BIAS_x_2),int(BIAS_y_1):int(BIAS_y_2)]
-			MASTER_BIAS_std=MASTER_BIAS_std[int(BIAS_x_1):int(BIAS_x_2),int(BIAS_y_1):int(BIAS_y_2)]
-		except:
-			pass
-	
-		y, x = np.mgrid[slice(0,len(MASTER_BIAS[:,0]), 1) ,slice(0, len(MASTER_BIAS[0,:]), 1) ]
-		cmap = plt.get_cmap('hot')
-	
-		fig, ax = plt.subplots()
-		axoff_fun(ax)
+		global t_fit
+		global y_fit
 		
-		im = ax.pcolormesh(x, y, MASTER_BIAS, cmap=cmap)
-		fig.colorbar(im, ax=ax)
-
-		fig.set_size_inches(4.5,4.5)
-		
-		self.canvas = FigureCanvasTkAgg(fig, master=master)  # A tk.DrawingArea.
-		self.canvas.draw()
-		self.canvas.get_tk_widget().pack(side=TOP, fill=BOTH, expand=1)
-	
-		#toolbar = NavigationToolbar2Tk(canvas, master)
-		#toolbar.update()
-		self.canvas.get_tk_widget().pack(side=TOP, fill=BOTH, expand=1)
-		self.canvas.get_tk_widget().place(x=400,y=150)
-		try:
-			plt.close(fig)
-		except:
-			pass
-		#plt.show()
-		
-	def bias_std(*event):
-		#destroy previous canvas to save memory
-		try:
-			_=self.canvas.get_tk_widget().destroy()
-		except:
-			pass
+		LIGHTCURVE_list=self.LIGHTCURVE_get.get()
+		LIGHTCURVE_images=read_list(LIGHTCURVE_list)
+		LIGHTCURVE_extension=self.EXTENSION_get.get()
+		LIGHTCURVE_times=self.TIME_COLUMN_get.get()
+		if self.INTENSITY_COLUMN_get.get()=='':
+			LIGHTCURVE_intensity=0
+		else:
+			LIGHTCURVE_intensity=self.INTENSITY_COLUMN_get.get()
 			
-		global MASTER_BIAS_std
+		if LIGHTCURVE_intensity==0:
+			for j in range(len(LIGHTCURVE_images)):
+				if j==0:
+					time_data=np.array(read_table(LIGHTCURVE_images[j],LIGHTCURVE_extension,LIGHTCURVE_times,0))
+					intensity_data=np.ones(len(time_data))
+				else:
+					time_data,intensity_data=np.dstack((LIGHTCURVE_data,np.array(read_table(LIGHTCURVE_images[j],LIGHTCURVE_extension,LIGHTCURVE_times,LIGHTCURVE_intensity))))
+		else:
+			for j in range(len(LIGHTCURVE_images)):
+				if j==0:
+					time_data,intensity_data=np.array(read_table(LIGHTCURVE_images[j],LIGHTCURVE_extension,LIGHTCURVE_times,LIGHTCURVE_intensity))
+				else:
+					print('Only the first fits was analyzed')
 		
-		y, x = np.mgrid[slice(0,len(MASTER_BIAS_std[:,0]), 1) ,slice(0, len(MASTER_BIAS_std[0,:]), 1) ]
-		cmap = plt.get_cmap('hot')
-	
+		
+		#aqui van las cosas
+		try:
+			T_PERIOD=float(self.TPERIOD_get.get())
+			period=T_PERIOD
+		except:
+			try:
+				min_period=float(self.MIN_PERIOD_get.get())
+				max_period=float(self.MAX_PERIOD_get.get())
+				bin_period=int(self.PERIOD_BINS_get.get())
+				results=BoxLeastSquares(time_data,intensity_data).power(np.linspace(min_period,max_period,bin_period),0.04) #.autopower(4) #LombScargle(time_data,intensity_data).autopower()
+				period=results.period[np.argmax(results.power)]
+				transit_time=results.transit_time[np.argmax(results.power)]
+			
+				best_frequency= 1.0/period#frequency[np.argmax(power)]
+				self.TPERIOD_get.delete(0,'end')
+				self.TPERIOD_get.insert(0,str(1.0/best_frequency))
+			except:
+				period=1.11
+		try:
+			T_START=float(self.TSTART_get.get())
+			if T_START >0:
+				while T_START>period:
+					T_START=T_START-period
+			elif T_START <0:
+				while T_START<0:
+					T_START=T_START+period
+			#time_data=time_data-T_START
+		except:
+			T_START=0
+		try:
+			nbins=int(self.NBINS_get.get())
+		except:
+			nbins=50
+		START_BIN=int(T_START*nbins/period)
+		period=period*u.day
+		cvcc=Time(val=time_data,format='mjd')
+		tabla_time = Table({'data':np.array(intensity_data,dtype=float)})
+		ts=TimeSeries(data=tabla_time,time=cvcc)
+		ts_fold=ts.fold(period=period)
+		TTG=Time(T_START,format='mjd')
+		ts_binned=aggregate_downsample(time_series=ts_fold,time_bin_size=period/nbins) 
+		
+		y_fit=np.append(ts_binned['data'][START_BIN:len(ts_binned['data'])],ts_binned['data'][0:START_BIN])
+		y_fit=np.append(y_fit,y_fit)
 		fig, ax = plt.subplots()
-		axoff_fun(ax)
+		#axoff_fun(ax)
+		ax.yaxis.set_label_position('right')
+		ax.xaxis.set_label_position('top')
+		ax.set_ylabel('Intensity (counts)')
+		ax.set_xlabel('Phase')
+		t_fit=np.linspace(0,2,2*len(ts_binned['data']))
+		ax.step(t_fit,y_fit,where='mid',color='k') #ts_binned.time_bin_start.jd
+		#ax.plot(ts_binned.time_bin_start.jd,ts_binned['data'],'k-',drawstyle='steps-post')
+		#ax.step(t_fit,y_fit,where='mid',color='k')
 		
-		im = ax.pcolormesh(x, y, MASTER_BIAS_std, cmap=cmap)
-		fig.colorbar(im, ax=ax)	
+		
+		#plt.errorbar(Pulse_profiles[j].ph,Pulse_profiles[j].profilenorm,yerr=Pulse_profiles[j].profile_err,fmt=color[j]+'o',markersize=0.5)
+
+	
+		#ax.plot(t_fit,y_fit,'k')
+		
+
 		fig.set_size_inches(4.5,4.5)
 		
-		#fig.savefig('test2png.png', dpi=100)
 		self.canvas = FigureCanvasTkAgg(fig, master=master)  # A tk.DrawingArea.
 		self.canvas.draw()
 		self.canvas.get_tk_widget().pack(side=TOP, fill=BOTH, expand=1)
 	
-		#toolbar = NavigationToolbar2Tk(canvas, master)
-		#toolbar.update()
+		toolbar = NavigationToolbar2Tk(self.canvas, master)
+		toolbar.update()
+		
+		self.canvas.toolbar.place(x=900,y=430)
+		self.canvas.get_tk_widget().pack(side=TOP, fill=BOTH, expand=1)
+		self.canvas.get_tk_widget().place(x=400,y=150)
 		self.canvas.get_tk_widget().pack(side=TOP, fill=BOTH, expand=1)
 		self.canvas.get_tk_widget().place(x=400,y=150)
 		try:
@@ -139,6 +268,12 @@ def LIGHTCURVE(self,master):
 		except:
 			pass
 		#plt.show()
+		
+		#display information
+		print('\n \n Lightcurve')
+		print('----------')
+		print('period= (', period/u.day ,')')
+	
 
 	fig, ax = plt.subplots()
 	fig.set_size_inches(4.5, 4.5)
@@ -146,36 +281,46 @@ def LIGHTCURVE(self,master):
 	# ... stuff here ...
 	axoff_fun(ax)
 	#fig.savefig('test2png.png', dpi=100)
-	self.Canvas_text=Label(master,text='CURRENTLY IMAGE',width=64,bg='grey')
+	self.Canvas_text=Label(master,text='CURRENTLY TABLE',width=64,bg='grey')
 	self.Canvas_text.pack()
 	self.Canvas_text.place(x=400,y=100)
 	self.canvas = FigureCanvasTkAgg(fig, master=master)  # A tk.DrawingArea.
 	self.canvas.draw()
-	#toolbar = NavigationToolbar2Tk(canvas, master)
-	#toolbar.update()
 	self.canvas.get_tk_widget().place(x=400,y=150)
 	
-
-
-	combining = StringVar(master)
-	combining.set("average") # initial value
-
-	self.combining_text=Label(master,text='Combining method',width=20,bg='grey')
-	self.combining_text.pack()
-	self.combining_text.place(x=10,y=300,height=30)
-	self.combining_get = OptionMenu(master, combining, 'average', 'median')
-	self.combining_get.config(width=10,bg='grey',highlightthickness=0)
-	self.combining_get.pack()
-	self.combining_get.place(x=200,y=300,height=30)
+	toolbar = NavigationToolbar2Tk(self.canvas, master)
+	toolbar.update()
+		
+	self.canvas.toolbar.place(x=900,y=430)
+	self.canvas.get_tk_widget().pack(side=TOP, fill=BOTH, expand=1)
+	self.canvas.get_tk_widget().place(x=400,y=150)
 	
-	def save_image(*event):
-		global MASTER_BIAS
+	try:
+		plt.close(fig)
+	except:
+		pass
+		
+	self.toolbar_text=Label(master,text='Toolbar',width=40,bg='grey')
+	self.toolbar_text.pack()
+	self.toolbar_text.place(x=900,y=400)
+
+	def save_lightcurve(*event):
+		global t_fit
+		global y_fit
 		name=self.image_name.get()
-		hdu = fits.PrimaryHDU(MASTER_BIAS)
-		t = fits.HDUList([hdu])
-		t.writeto(name+'.fits',overwrite=True)
+		
+		
+		try:
+			c1 = fits.Column(name='phase',array=t_fit, format='E')
+			c2 = fits.Column(name='intensity',array=y_fit, format='E')
+			#c3 = fits.Column(name='intensity',array=MASTER_IMAGE, format='E')
+			t = fits.BinTableHDU.from_columns([c1,c2],name='VALUES')
+			t.writeto(name+'.fits',overwrite=True)
+			
+		except:
+			print('no available lightcurve to save')
 	
-	self.save_image_text=Button(master,text='Save master bias as',width=20,bg='grey',command=save_image)
+	self.save_image_text=Button(master,text='Save lightcurve as',width=20,bg='grey',command=save_lightcurve)
 	self.save_image_text.pack()
 	self.save_image_text.place(x=400,y=620)
 	self.image_name=Entry(master,bg='grey')
@@ -183,47 +328,11 @@ def LIGHTCURVE(self,master):
 	self.image_name.pack()
 	self.image_name.place(x=560,y=620,width=200,height=30)
 	
-	self.combining_button=Button(master,text='Make master bias',width=20,command=LIGHTCURVE,bg='grey')
+	self.combining_button=Button(master,text='Make lightcurve',width=20,command=lightcurve,bg='grey')
 	self.combining_button.pack()
 	self.combining_button.place(x=10,y=590)
 	
-	self.show_std_button=Button(master,text='Show standard deviation',width=20,command=bias_std,bg='grey')
-	self.show_std_button.pack()
-	self.show_std_button.place(x=180,y=590)
 	
-	self.ZOOM_text=Label(master,text='Image zoom',width=40,bg='grey')
-	self.ZOOM_text.pack()
-	self.ZOOM_text.place(x=900,y=470)
 	
-	self.Xmin_text=Label(master,text='X min',bg='grey')
-	self.Xmin_text.pack()
-	self.Xmin_text.place(x=900,y=500,width=60,height=30)
-	self.Xmin_value=Entry(master,bg='grey')
-	self.Xmin_value.insert(0,'')
-	self.Xmin_value.pack()
-	self.Xmin_value.place(x=975,y=500,width=60,height=30)
-	
-	self.Xmax_text=Label(master,text='X max',bg='grey')
-	self.Xmax_text.pack()
-	self.Xmax_text.place(x=1050,y=500,width=60,height=30)
-	self.Xmax_value=Entry(master,bg='grey')
-	self.Xmax_value.insert(0,'')
-	self.Xmax_value.pack()
-	self.Xmax_value.place(x=1125,y=500,width=60,height=30)
-	
-	self.Ymin_text=Label(master,text='Y min',bg='grey')
-	self.Ymin_text.pack()
-	self.Ymin_text.place(x=900,y=550,width=60,height=30)
-	self.Ymin_value=Entry(master,bg='grey')
-	self.Ymin_value.insert(0,'')
-	self.Ymin_value.pack()
-	self.Ymin_value.place(x=975,y=550,width=60,height=30)
-	
-	self.Ymax_text=Label(master,text='Y max',bg='grey')
-	self.Ymax_text.pack()
-	self.Ymax_text.place(x=1050,y=550,width=60,height=30)
-	self.Ymax_value=Entry(master,bg='grey')
-	self.Ymax_value.insert(0,'')
-	self.Ymax_value.pack()
-	self.Ymax_value.place(x=1125,y=550,width=60,height=30)
+
 
